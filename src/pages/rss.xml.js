c4fd204ad83b3be2985917fd97367b5912817ade
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-10-04 12:52:48 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Web Development and Technology Blog | Fernando López | EFEELE',
-    description: 'Welcome to my blog, where I share my passion for frontend development, web design, and the latest technology trends.',
+    title: 'Dopi 不脱妆粉底液| Mia Wang | 米娅',
+    description: ' 我是卖美妆创业的米娅, 一个混迹于抖音、视频号、小红书的自由主播。',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es</language>`,
